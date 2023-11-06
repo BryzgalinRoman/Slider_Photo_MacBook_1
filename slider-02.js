dff67369705934b1ae2a6bd8eaeef9765152c29d
BryzgalinRoman <rbryzgalin@gmail.com>
2023-11-06 14:48:11 +0700

    Update slider-02.js

---
+++ slider-02.js
@@ -0,0 +1,27 @@
+//Объявили ассоциативный массив images
+
+const images = [{
+    url: "./images/png/slider_photo_1.png",
+    title: "Rostov-on-Don, Admiral",
+    city: "Rostov-on-Don",
+    apart: "LCD admiral",
+    area: "81 m2",
+    time: "3.5 months"
+  }, {
+    url: "./images/png/slider_photo_2.png",
+    title: "Sochi Thieves",
+    city: "Sochi",
+    apart: "Thieves",
+    area: "105 m2",
+    time: "4 months"
+  }, {
+    url: "./images/png/slider_photo_3.png",
+    title: "Rostov-on-Don Patriotic",
+    city: "Rostov-on-Don",
+    apart: "Patriotic",
+    area: "93 m2",
+    time: "3 months"
+}]; 
+
+
+  
